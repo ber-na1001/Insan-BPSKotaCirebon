7f0ae1e64a35469e6df90d86d98ef608e5ecaedb
ber-na1001 <berlianaputri1001@gmail.com>
2024-10-30 13:20:26 +0700

    tentang_inflasi.py

--- tentang_inflasi.py
+++ tentang_inflasi.py
@@ -12,5 +12,8 @@ def show():
 
         ### Inflasi menurut Month-to-Month (MtM)
         **Inflasi MtM** mengukur perubahan harga dari bulan ke bulan. Misalnya, jika harga barang dan jasa pada bulan Agustus adalah 102 dan pada bulan September adalah 105, maka inflasi MtM adalah 2,94% [(105-102)/102 x 100]. Pengukuran ini memberikan pandangan yang lebih langsung dan responsif terhadap perubahan harga dalam waktu singkat.
+
+        ### Inflasi Year-to-Date (YtD)
+        **Inflasi YtD** mengukur persentase perubahan harga dari awal tahun hingga periode tertentu dalam tahun yang sama. Misalnya, jika harga barang dan jasa pada Januari adalah 100 dan pada September mencapai 105, maka inflasi YtD adalah 5%. Metode pengukuran ini bermanfaat untuk memantau perubahan harga kumulatif sepanjang tahun berjalan, memberikan perspektif tentang dinamika inflasi yang terjadi dalam kurun waktu lebih pendek dibandingkan inflasi tahunan (YoY).
         """
     )
